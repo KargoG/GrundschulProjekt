3572b8dce3c444e7f9b70e4d1027dc4e58841338
Maotelus <30568783+Maotelus@users.noreply.github.com>
2017-09-05 19:42:54 +0200

    überarbietet
    das ist alles

---
+++ Credits/Credits.js
@@ -0,0 +1,10 @@
+$(function(){
+
+	$("#exit").css({"background":"green"});
+
+	$("#exit").css({"cursor":"pointer"});
+	
+	$("#exit").click(function(){
+		location.href="../Startseite/Startseite.html";//hier relativ adressierung zur Startseite
+	});
+});
